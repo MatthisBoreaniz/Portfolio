90c9001bd485911a414219cadbbc567cdbb1196c
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-11-15 10:09:34 +0100

    mise en page simple de projet [id]*

--- src/pages/projets/[id].vue
+++ src/pages/projets/[id].vue
@@ -1,6 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import HeaderMenu from '@/components/HeaderMenu.vue';
+</script>
+
+
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <h1>
-        Projet
-    </h1>
+    <div class="bg-animate">
+        <HeaderMenu />
+        <h1 class="font-calistoa text-xl text-center"> Voici le projet</h1>
+
+
+    </div>
 </template>
