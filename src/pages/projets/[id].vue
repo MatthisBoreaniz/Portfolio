605a4130560076205697c01189be380482e429bc
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-11-15 10:37:54 +0100

    forme peojet

--- src/pages/projets/[id].vue
+++ src/pages/projets/[id].vue
@@ -1,14 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { pb } from '@/backend';
 import HeaderMenu from '@/components/HeaderMenu.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute('/projets/[id]')
+
+const Projets = await pb.collection('projets').getOne(route.params.id)
 </script>
 
 
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="bg-animate">
+    <div class="bg-animate h-screen">
         <HeaderMenu />
-        <h1 class="font-calistoa text-xl text-center"> Voici le projet</h1>
+        <h1 class="font-calistoga text-white text-xl text-center"> Voici le projet {{ Projets.titre }}</h1>
 
 
     </div>
