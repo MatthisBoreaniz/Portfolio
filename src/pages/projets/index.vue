a5a0c4f4b7d495c3dae75dbd497cef4db516356d
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-10 16:49:50 +0100

    redirection vers les projet[id] sur la page projets

--- src/pages/projets/index.vue
+++ src/pages/projets/index.vue
@@ -2,17 +2,25 @@
 <script setup lang="ts">
 import { pb } from '@/backend';
 import cardProjet from '@/components/cardProjet.vue';
+import HeaderMenu from '@/components/HeaderMenu.vue';
+import { RouterLink } from 'vue-router';
 
 const listProjets = await pb.collection('projets').getFullList();
 </script>
 
 <template>
 
-<div>
-
+<div class="bg-animate">
+    <HeaderMenu />
     <h1>Les projets </h1>
         <div v-for="unProjet in listProjets" :key="unProjet.id">
-            <cardProjet v-bind="unProjet" />
+            <RouterLink :to="{
+                name: '/projets/[id]',
+                params: { id: unProjet.id }
+            }">
+                <cardProjet v-bind="unProjet" />
+            </RouterLink>
+                
         </div>
    
 </div>
