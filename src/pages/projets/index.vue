7b3b168ccce88dc257df92ac8852d85a9ec76fd0
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-12 22:22:19 +0100

    bouton creer projet css

--- src/pages/projets/index.vue
+++ src/pages/projets/index.vue
@@ -25,21 +25,21 @@ function edit () {
 <div class="bg-animate min-h-screen">
     <HeaderMenu />
     <h1 class="font-calistoga text-base flex items-center justify-center text-white py-10">Mes projets </h1>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4 justify-center">
-            <div v-for="unProjet in listProjets" :key="unProjet.id" class="p-2 flex justify-center">
-                <RouterLink :to="{
-                    name: '/projets/[id]',
-                    params: { id: unProjet.id }
-                }">
-                    <cardProjet v-bind="unProjet" />
-                </RouterLink>
-            </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-4 p-4 justify-center">
+        <div v-for="unProjet in listProjets" :key="unProjet.id" class="p-2 flex justify-center">
+            <RouterLink :to="{
+                name: '/projets/[id]',
+                params: { id: unProjet.id }
+            }">
+                <cardProjet v-bind="unProjet" />
+            </RouterLink>
         </div>
+    </div>
    
-</div>
-
-<div class="flex justify-center" v-if="user">
-    <button @click="edit" class="bg-white text-black font-calistoga text-base p-2 rounded-lg mt-4">Créer un projet</button>
-</div>
-
+        
+        <div class="flex justify-center" v-if="user">
+            <button @click="edit" class="text-white font-calistoga hover:bg-white hover:text-black text-sm bg-Black border-2 p-2 rounded-lg mt-4">Créer un projet</button>
+        </div>
+        
+    </div>
 </template>
