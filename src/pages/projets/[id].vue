bb58097a6e866775b4e3357a3d973cfcaa2bff22
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-12 15:28:45 +0100

    probleme page id resolu

--- src/pages/projets/[id].vue
+++ src/pages/projets/[id].vue
@@ -8,26 +8,29 @@ import sanitizeHtml from 'sanitize-html'
 import { ref } from 'vue'
 const route = useRoute('/projets/[id]')
 
+
 const Projets = await pb.collection('projets').getOne(route.params.id)
 const photos = [
   { id: Projets.id, filename: Projets.photo1, record: Projets },
   { id: Projets.id, filename: Projets.photo2, record: Projets },
   { id: Projets.id, filename: Projets.photo3, record: Projets }
-].filter((photo) => photo.filename)
+].filter(photo => photo.filename);
 
-const currentIndex = ref(1)
+
+const currentIndex = ref(1);
+
 
 const nextSlide = () => {
-  currentIndex.value = (currentIndex.value + 1) % photos.length
-}
+  currentIndex.value = (currentIndex.value + 1) % photos.length;
+};
 
 const prevSlide = () => {
-  currentIndex.value = (currentIndex.value - 1 + photos.length) % photos.length
-}
+  currentIndex.value = (currentIndex.value - 1 + photos.length) % photos.length;
+};
 
 const goToSlide = (index: number) => {
-  currentIndex.value = index
-}
+      currentIndex.value = index;
+};
 </script>
 
 <template>
@@ -40,64 +43,60 @@ const goToSlide = (index: number) => {
       <ImgPb :record="Projets" :filename="Projets.logo" class="mx-auto max-w-full object-cover" />
     </div>
     <div class="h-screen">
-      <ImgPb :record="Projets" :filename="Projets.image" class="h-full w-full object-cover" />
+      <ImgPb
+        :record="Projets"
+        :filename="Projets.image"
+        class="h-full w-full object-cover "
+      />
     </div>
-    <div class="flex flex-col items-center justify-center">
-      <div
-        class="w-2/3 p-10 font-calistoga text-white"
-        v-html="sanitizeHtml(Projets.description1)"
-      ></div>
-      <div
-        class="w-2/3 p-10 font-calistoga text-white"
-        v-html="sanitizeHtml(Projets.description2)"
-      ></div>
-      <div
-        class="w-2/3 p-10 font-calistoga text-white"
-        v-html="sanitizeHtml(Projets.description3)"
-      ></div>
+    <div class="flex flex-col justify-center items-center ">
+      <div class="p-10 w-2/3 font-calistoga text-white" v-html="sanitizeHtml(Projets.description1)"></div>
+      <div class="p-10 w-2/3 font-calistoga text-white" v-html="sanitizeHtml(Projets.description2)"></div>
+      <div class="p-10 w-2/3 font-calistoga text-white" v-html="sanitizeHtml(Projets.description3)"></div>
     </div>
+    
+  </div>
 
-    <div class="flex min-h-screen items-center justify-center">
-      <div class="relative mx-auto w-full max-w-4xl overflow-hidden py-10">
-        <div class="relative flex h-96 items-center justify-center">
-          <div
-            v-for="(photo, index) in photos"
-            :key="photo.id + photo.filename"
-            class="absolute cursor-pointer rounded-lg shadow-lg transition-transform duration-500 ease-in-out"
-            :style="{ transform: `translateX(${(index - currentIndex) * 100}%)` }"
-            @click="goToSlide(index)"
-          >
-            <ImgPb
-              :record="photo.record"
-              :filename="photo.filename"
-              class="h-auto w-[500px] rounded-lg object-contain"
-            />
-          </div>
+
+ <div class="flex justify-center items-center min-h-screen bg-animate">
+    <div class="relative w-full max-w-4xl mx-auto overflow-hidden py-10">
+      <div class="relative flex items-center justify-center h-96">
+        <div
+          v-for="(photo, index) in photos"
+          :key="photo.id + photo.filename"
+          class="absolute transition-transform duration-500 ease-in-out rounded-lg shadow-lg cursor-pointer"
+          :style="{ transform: `translateX(${(index - currentIndex) * 100}%)` }"
+          @click="goToSlide(index)"
+        >
+          <ImgPb
+            :record="photo.record"
+            :filename="photo.filename"
+            class="w-[500px] h-auto object-contain rounded-lg"
+          />
         </div>
+      </div>
 
-        <div class="flex justify-around">
+      <div class="flex justify-around ">
           <button
             @click="prevSlide"
-            class="top-0 z-30 -translate-y-1/2 transform rounded-full bg-gray-100 p-3 text-black shadow-lg hover:bg-gray-300"
+            class="top-0 transform -translate-y-1/2 bg-gray-100 hover:bg-gray-300 text-black rounded-full p-3 shadow-lg z-30"
           >
             ‹
           </button>
           <button
             @click="nextSlide"
-            class="top-0 z-30 -translate-y-1/2 transform rounded-full bg-gray-100 p-3 text-black shadow-lg hover:bg-gray-300"
+            class="top-0  transform -translate-y-1/2 bg-gray-100 hover:bg-gray-300 text-black rounded-full p-3 shadow-lg z-30"
           >
             ›
           </button>
-        </div>
       </div>
     </div>
   </div>
 </template>
+
 <style scoped>
 button {
-  transition:
-    background-color 0.3s,
-    transform 0.3s;
+  transition: background-color 0.3s, transform 0.3s;
 }
 
 button:hover {
