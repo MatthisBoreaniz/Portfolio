5883fac9caaa59e4f079a39e6882d526f56c8d9b
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-16 15:09:57 +0100

    ajout d'un message de validité pour mail

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -19,6 +19,8 @@ const form = ref<FormData>({
   message: ''
 })
 
+const notification = ref<string | null>(null)
+
 const submitForm = async () => {
   try {
     const response = await fetch('https://api.web3forms.com/submit', {
@@ -36,25 +38,30 @@ const submitForm = async () => {
     const result = await response.json()
 
     if (result.success) {
-      console.log('Form submitted successfully:', result)
-      // Reset the form if needed
-      form.value = { firstName: '', lastName: '', email: '', message: '' }
+      notification.value = 'Votre message a été envoyé avec succès.'
+      form.value = { firstName: '', lastName: '', email: '', message: '' } // Reset the form
     } else {
-      console.error('Form submission failed:', result)
+      notification.value = "Une erreur est survenue lors de l'envoi du message."
     }
   } catch (error) {
-    console.error('Error submitting form:', error)
+    notification.value = "Une erreur est survenue lors de l'envoi du message."
   }
+  setTimeout(() => {
+    notification.value = null
+  }, 2000)
 }
 </script>
 
 <template>
-  <div class="min-h-screen bg-black overflow-x-hidden" style="min-height: 100vh; position: relative">
+  <div
+    class="min-h-screen overflow-x-hidden bg-black"
+    style="min-height: 100vh; position: relative"
+  >
     <HeaderMenu class="z-50" />
 
     <div class="hidden lg:block">
       <Elipse
-        class="absolute top-50  z-0"
+        class="top-50 absolute z-0"
         style="
           width: 1500px;
           height: 1500px;
@@ -66,70 +73,101 @@ const submitForm = async () => {
       />
     </div>
 
-    <div class="flex w-full flex-col p-4 z-10">
-      <div class="flex items-center mb-4">
-        <h1 class="inline-block whitespace-nowrap rounded-r-3xl bg-DeepBlue p-5 font-calistoga lg:text-lg text-base text-white">
+    <div class="z-10 flex w-full flex-col p-4">
+      <div class="mb-4 flex items-center">
+        <h1
+          class="inline-block whitespace-nowrap rounded-r-3xl bg-DeepBlue p-5 font-calistoga text-base text-white lg:text-lg"
+        >
           Contactez moi
         </h1>
         <div class="h-1 w-full bg-white"></div>
       </div>
-      <section class="flex flex-col lg:flex-row justify-center lg:gap-24">
-        <div class="flex flex-col justify-center items-center mb-10 lg:mb-0">
-          <h2 class="font-calistoga text-base text-white py-10">Via mes réseaux </h2>
+      <section class="flex flex-col justify-center lg:flex-row lg:gap-24">
+        <div class="mb-10 flex flex-col items-center justify-center lg:mb-0">
+          <h2 class="py-10 font-calistoga text-base text-white">Via mes réseaux</h2>
           <div>
             <ul class="flex flex-col items-center justify-center space-y-5">
               <li class="flex items-center justify-center space-x-3">
-                <img class="h-[50px] w-[50px]" src="/src/assets/Img/DossierIcons/icons8-linkedin 1.png" alt="LinkedinIcon" />
-                <a href="https://www.linkedin.com/in/matthis-boreaniz-70879a281/" class="font-calistoga text-sm text-white z-10">MatthisBoreaniz</a>
+                <img
+                  class="h-[50px] w-[50px]"
+                  src="/src/assets/Img/DossierIcons/icons8-linkedin 1.png"
+                  alt="LinkedinIcon"
+                />
+                <a
+                  href="https://www.linkedin.com/in/matthis-boreaniz-70879a281/"
+                  class="z-10 font-calistoga text-sm text-white"
+                  >MatthisBoreaniz</a
+                >
               </li>
               <li class="flex items-center justify-center space-x-3">
-                <img class="h-[50px] w-[50px]" src="/src/assets/Img/DossierIcons/icons8-github 1.png" alt="GithubIcon" />
-                <a href="https://github.com/MatthisBoreaniz" class="font-calistoga text-sm text-white z-10">MatthisBoreaniz</a>
+                <img
+                  class="h-[50px] w-[50px]"
+                  src="/src/assets/Img/DossierIcons/icons8-github 1.png"
+                  alt="GithubIcon"
+                />
+                <a
+                  href="https://github.com/MatthisBoreaniz"
+                  class="z-10 font-calistoga text-sm text-white"
+                  >MatthisBoreaniz</a
+                >
               </li>
               <li class="flex items-center justify-center space-x-3">
-                <img class="h-[50px] w-[50px]" src="/src/assets/Img/DossierIcons/icons8-insta 1.png" alt="InstagramIcon" />
-                <a href="https://www.instagram.com/matthisboreaniz/" class="font-calistoga text-sm text-white z-10">MatthisBoreaniz</a>
+                <img
+                  class="h-[50px] w-[50px]"
+                  src="/src/assets/Img/DossierIcons/icons8-insta 1.png"
+                  alt="InstagramIcon"
+                />
+                <a
+                  href="https://www.instagram.com/matthisboreaniz/"
+                  class="z-10 font-calistoga text-sm text-white"
+                  >MatthisBoreaniz</a
+                >
               </li>
             </ul>
           </div>
         </div>
-        <div class="w-full lg:w-[700px] rounded-r-lg p-6 shadow-lg">
-          <h2 class="font-calistoga text-base text-center text-white py-10">Ou mon formulaire ! </h2>
+        <div class="w-full rounded-r-lg p-6 shadow-lg lg:w-[700px]">
+          <h2 class="py-10 text-center font-calistoga text-base text-white">Ou mon formulaire !</h2>
           <form @submit.prevent="submitForm" class="flex flex-col gap-6">
             <input
               type="text"
               name="firstName"
               v-model="form.firstName"
               placeholder="Votre Prénom"
-              class="border-2px w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500 z-10"
+              class="border-2px z-10 w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500"
             />
             <input
               type="text"
               name="lastName"
               v-model="form.lastName"
               placeholder="Votre Nom"
-              class="border-2px w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500 z-10"
+              class="border-2px z-10 w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500"
             />
             <input
               type="email"
               name="email"
               v-model="form.email"
               placeholder="Votre Email"
-              class="border-2px w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500 z-10"
+              class="border-2px z-10 w-full rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500"
             />
             <textarea
               name="message"
               v-model="form.message"
               placeholder="Votre Message"
-              class="border-2px h-32 w-full resize-none rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500 z-10"
+              class="border-2px z-10 h-32 w-full resize-none rounded-2xl border border-DeepBlue bg-white p-2 text-black placeholder-gray-500"
             ></textarea>
             <button
               type="submit"
-              class="self-end rounded-lg bg-DeepBlue px-2 py-1 font-calistoga text-sm text-white transition hover:bg-purple-700 z-10"
+              class="z-10 self-end rounded-lg bg-DeepBlue px-2 py-1 font-calistoga text-sm text-white transition hover:bg-purple-700"
             >
               Envoyer
             </button>
           </form>
+            <div v-if="notification" class="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-center w-full">
+            <div class="rounded-lg bg-DeepBlue p-4 text-white shadow-lg">
+              {{ notification }}
+            </div>
+            </div>
         </div>
       </section>
     </div>
