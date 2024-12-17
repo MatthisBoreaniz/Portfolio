f7a9ca8a27e2a53006e556c05d61bff7d8f7d2f6
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-17 10:47:48 +0100

    Remove CV.pdf and update download link in footer to use 'download' attribute

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,9 @@
 <footer class="bg-black py-4 font-calistoga text-white border-t-2 border-white">
     <div class="container mx-auto text-center">
       <p>&copy; 2024 Matthis Boreaniz. Tous droits réservés</p>
-      <a href="/CV.pdf" target="_blank" class="text-white underline hover:text-DeepBlue"
-        ><button>Télécharger mon CV</button></a
-      >
+       <a href="/CV.pdf" download="cv_matthis_boreaniz.pdf" class="text-white underline hover:text-DeepBlue">
+        Télécharger mon CV
+      </a>
       <section class="flex justify-center pt-2 gap-4">
         <a href="https://github.com/MatthisBoreaniz"><img class="w-[50px] h-[50px]" src="/src/assets/Img/DossierIcons/icons8-github 1.png" alt="Icone Github" /></a>
         <a href="https://www.linkedin.com/in/matthis-boreaniz-70879a281/"><img class="w-[50px] h-[50px]" src="/src/assets/Img/DossierIcons/icons8-linkedin 1.png" alt="Icone Linkedin" /></a>
