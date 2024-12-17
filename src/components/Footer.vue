2adf033f1db90a3064381ae50f6c425816bc034d
MatthisBoreaniz <matthis.boreaniz@edu.univ-fcomte.fr>
2024-12-17 08:31:25 +0100

    Footer Mis en place

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-
-
-</script>
-
+<script setup lang="ts"></script>
 
 <template>
-    <footer class="bg-black font-calistoga text-white py-4">
-        <div class="container mx-auto text-center">
-            <p>&copy; 2024 Matthis Boreaniz. Tous droits réservés</p>
-            <a href="/src/assets/Img/CV.pdf" download class="text-DeepRed hover:text-blue-600">Télécharger mon CV</a>
-        </div>
-    </footer>
+<footer class="bg-black py-4 font-calistoga text-white border-t-2 border-white">
+    <div class="container mx-auto text-center">
+      <p>&copy; 2024 Matthis Boreaniz. Tous droits réservés</p>
+      <a href="/src/assets/Img/CV.pdf" download class="text-White underline hover:text-blue-600"
+        >Télécharger mon CV</a
+      >
+      <section class="flex justify-center pt-2 gap-4">
+        <a href="https://github.com/MatthisBoreaniz"><img class="w-[50px] h-[50px]" src="/src/assets/Img/DossierIcons/icons8-github 1.png" alt="Icone Github" /></a>
+        <a href="https://www.linkedin.com/in/matthis-boreaniz-70879a281/"><img class="w-[50px] h-[50px]" src="/src/assets/Img/DossierIcons/icons8-linkedin 1.png" alt="Icone Linkedin" /></a>
+        <a href="https://www.instagram.com/matthisboreaniz/"><img class="w-[50px] h-[50px]" src="/src/assets/Img/DossierIcons/icons8-insta 1.png" alt="icone Instagram" /></a>
+    </section>
+    </div>
+  </footer>
 </template>
